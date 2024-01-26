40f2e3e4c544eb1e8b2c3361a098e47376cd4589
zhongyulyu <zhongyu.lyu@icloud.com>
2024-01-26 22:17:34 +0800

    CorrectBase

---
+++ calculate/correct.py
@@ -0,0 +1,44 @@
+from expression_check import CheckExpression
+class Correct:
+    # FIRST_OPERATOR_ERROR = I
+    # LAST_OPERATOR_ERROR = II
+    # MISMATCHED_LFT_BRACKET_ERROR = III
+    # MISMATCHED_RGH_BRACKET_ERROR = IV
+    # DIVIDE_ZERO_ERROR = V
+    # CONTINUOUS_OPERATOR_ERROR = VI
+    # others is unused
+    def __init__(self):
+        pass
+
+    def type_I(self):
+        pass
+
+    def type_II(self):
+        pass
+
+    def type_III(self):
+        pass
+
+    def type_IV(self):
+        pass
+
+    def type_V(self):
+        pass
+
+    def type_VI(self):
+        pass
+
+    def type_VII(self):
+        pass
+
+    def type_VIII(self):
+        pass
+
+    def type_IX(self):
+        pass
+
+    def type_X(self):
+        pass
+
+    def main(self):
+        pass
